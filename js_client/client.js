3cd4087350fa0db84fe4c6832a25c907e7a34f3c
sippyyy <nguyenthuy030800@gmail.com>
2023-07-30 06:00:58 +0700

    add refresh token, and verify token

--- js_client/client.js
+++ js_client/client.js
@@ -24,38 +24,57 @@ function handleLogin(event) {
         return response.json()
     })
     .then(authData => {
-        handleAuthData(authData)
+        handleAuthData(authData,getProductList)
     })
     .catch(err=> {
         console.log('err', err)
     })
 }
 
-const handleAuthData = (data) => {
+const getFetchOptions = (method,body)=>{
+    const access_token = localStorage.getItem('access')
+    return{
+        method: method ? method : "GET",
+        headers:{
+            'Content-Type': "application/json",
+            Authorization:"Bearer " + access_token
+        },
+        body : body? body : null
+    }
+}
+
+
+const handleAuthData = (data,callback) => {
     localStorage.setItem('access',data.access)
     localStorage.setItem('refresh',data.refresh)
-    getProductList()
+    if(callback){
+        callback()
+    }
 }
+
+const isTokenValid = (code)=>{
+    if(code && code === "token_not_valid"){
+        localStorage.removeItem('access')
+        localStorage.removeItem('refresh')
+        alert("Please login again. Token is no longer valid")
+        return false
+    }
+    return true
+}
+
 
 const getProductList = ()=>{
     const product_endpoint = `${baseEndpoint}/products/`
-    const access_token = localStorage.getItem('access')
-    if(access_token) {
-        const options = {
-            method:"GET",
-            headers:{
-                'Content-Type': "application/json",
-                Authorization:"Bearer " + access_token
-            }
-        }
-        fetch(product_endpoint, options)
-        .then(res=>res.json())
-        .then(data=>{
+    const options = getFetchOptions()
+    fetch(product_endpoint, options)
+    .then(res=>res.json())
+    .then(data=>{
+        const validData = isTokenValid(data?.code??'')
+        if(validData) {
             renderProduct(data.results)
-        })
-        .catch(err=>console.log(err))
-
-    }
+        }
+    })
+    .catch(err=>console.log(err))
 }
 
 
@@ -64,8 +83,43 @@ const renderProduct = (data)=>{
         function generateListItemsHTML(data) {
             return data.map(product => `<li>${product.title}</li>`).join('');
           }
-          
-          // Set the innerHTML of the <ul> element with the generated HTML content
           productList.innerHTML = generateListItemsHTML(data);
     }
 }
+
+
+
+const validateJWTToken = ()=>{
+    const validate_endpoint = `${baseEndpoint}/token/verify/`
+    const bodyVerify = JSON.stringify({token : localStorage.getItem('access')})
+    const options = getFetchOptions('POST',bodyVerify);
+    fetch(validate_endpoint, options)
+    .then(res=>res.json())
+    .then(data => {
+        const token_valid = isTokenValid(data?.code??'')
+        if(token_valid) {
+            getProductList()
+        }else{
+            refreshToken()
+        }
+    })
+}
+
+const refreshToken = ()=>{
+    const refresh_endpoint = `${baseEndpoint}/token/refresh/`
+    const body = JSON.stringify({token : localStorage.getItem('refresh')})
+    const options = getFetchOptions('POST',body)
+    fetch(refresh_endpoint,options)
+    .then(res=>res.json())
+    .then(data=>{
+        const valid_token =  isTokenValid(data?.code??'')
+        if(valid_token){
+            localStorage.setItem('access',data.access)
+            localStorage.setItem('refresh',data.refresh)
+        }else{
+            alert("Please login again. Token is no longer valid")
+        }
+    })
+}
+
+validateJWTToken()
